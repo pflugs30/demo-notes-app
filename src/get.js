26e5535ba081b9825ba398e51e06d3aaf205b098
Matthew Pfluger <matthew.pfluger@kcc.com>
2022-04-02 12:03:46 -0500

    Adding a handler error

--- src/get.js
+++ src/get.js
@@ -1,12 +1,10 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-// Some faulty code
-dynamoDb.notExist();
-
-export const main = handler(async (event) => {
+// Wrong handler function name
+export const main2 = handler(async (event) => {
   const params = {
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
@@ -16,7 +14,7 @@ export const main = handler(async (event) => {
     },
   };
 
-  const result = await dynamoDb.get(params);
+  const result = await dynamoDb.call("get", params);
   if (!result.Item) {
     throw new Error("Item not found.");
   }
